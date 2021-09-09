ee2a12bb68216d58e11398f16492c7ef20476d76
Miguel Rios <miguelriosoliveira@gmail.com>
2021-09-08 22:58:09 -0300

    feature: Added test for CORS header

--- tests/unit/routes.test.ts
+++ tests/unit/routes.test.ts
@@ -4,26 +4,28 @@ import io from 'socket.io';
 
 import { Routes } from '../../src/routes';
 
+let routes: Routes;
+
+beforeEach(() => {
+	routes = new Routes();
+});
+
 describe('#Routes', () => {
 	describe('#setSocketInstance', () => {
 		test('it should store io server instance', () => {
-			// Arrange
-			const routes = new Routes();
 			const httpServer = http.createServer();
 			const ioServer = new io.Server(httpServer);
-
-			// Act
 			routes.setSocketInstance(ioServer);
-
-			// Assert
 			expect(routes.ioServer).toStrictEqual(ioServer);
 		});
 	});
 
 	describe('#handler', () => {
-		type IDefaultParams = [Partial<IncomingMessage>, Partial<ServerResponse>];
-
-		const defaultParams: IDefaultParams = [
+		type Request = Partial<IncomingMessage>;
+		type Response = Partial<ServerResponse>;
+		let request: Request;
+		let response: Response;
+		const defaultParams: [Request, Response] = [
 			{
 				method: '',
 				headers: { 'Content-Type': 'multipart/form-data' },
@@ -35,20 +37,29 @@ describe('#Routes', () => {
 			},
 		];
 
-		test('given an inexistent route, it should choose the default one', () => {
-			const routes = new Routes();
-			const [request, response] = [...defaultParams];
-			request.method = 'potato';
+		beforeEach(() => {
+			[request, response] = [...defaultParams];
+		});
 
+		test('it should enable CORS on all requests', () => {
 			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 			// @ts-ignore
 			routes.handler(request, response);
+			expect(response.setHeader).toHaveBeenCalledWith('Access-Control-Allow-Origin', '*');
+		});
 
+		test('given an inexistent route, it should choose the default one', () => {
+			request.method = 'potato';
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			routes.handler(request, response);
 			expect(response.end).toHaveBeenCalledWith('this is DEFAULT');
 		});
-		test.todo('it should enable CORS on all requests');
+
 		test.todo('given the method OPTIONS, it should choose the options route');
+
 		test.todo('given the method POST, it should choose the post route');
+
 		test.todo('given the method GET, it should choose the get route');
 	});
 });
